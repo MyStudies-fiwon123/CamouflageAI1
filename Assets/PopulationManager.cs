2124d0887f1a79a4bbf0545775aa038e033db1f2
fiwon123 <felipeinoue@yahoo.com.br>
2020-07-05 22:37:16 -0300

    Finish Camouflage

--- Assets/PopulationManager.cs
+++ Assets/PopulationManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PopulationManager : MonoBehaviour
@@ -15,8 +16,8 @@ public class PopulationManager : MonoBehaviour
 
     void OnGUI(){
         guiStyle.fontSize = 50;
-        guiStyle.normal.tetColor = Color.white;
-        GUI.Label(new React(10, 10, 100, 20), "Generaction: " + generation, guiStyle);
+        guiStyle.normal.textColor = Color.white;
+        GUI.Label(new Rect(10, 10, 100, 20), "Generaction: " + generation, guiStyle);
         GUI.Label(new Rect(10, 65, 100, 20), "Trial Time: " + (int)elapsed, guiStyle);
     }
 
@@ -31,6 +32,44 @@ public class PopulationManager : MonoBehaviour
             go.GetComponent<DNA>().b = Random.Range(0.0f, 1.0f);
             population.Add(go);
         }
+    }
+
+    GameObject Breed(GameObject parent1, GameObject parent2){
+        Vector3 pos = new Vector3(Random.Range(-9,9), Random.Range(-4.5f, 4.5f), 0);
+        GameObject offspring = Instantiate(personPrefab, pos, Quaternion.identity);
+        DNA dna1 = parent1.GetComponent<DNA>();
+        DNA dna2 = parent2.GetComponent<DNA>();
+        //swap parent dna
+        if (Random.Range(0, 1000) < 5)
+        {
+            offspring.GetComponent<DNA>().r = Random.Range(0, 10) < 5 ? dna1.r : dna2.r;
+            offspring.GetComponent<DNA>().g = Random.Range(0, 10) < 5 ? dna1.g : dna2.g;
+            offspring.GetComponent<DNA>().b = Random.Range(0, 10) < 5 ? dna1.b : dna2.b;
+        }else{
+            offspring.GetComponent<DNA>().r = Random.Range(0f, 1f);
+            offspring.GetComponent<DNA>().g = Random.Range(0f, 1f);
+            offspring.GetComponent<DNA>().b = Random.Range(0f, 1f);
+        }
+        return offspring;
+    }
+
+    private void BreedNewPopulation()
+    {
+        List<GameObject> newPopulation = new List<GameObject>();
+        // get rid of unfit individuals
+        List<GameObject> sortedList = population.OrderByDescending(o => o.GetComponent<DNA>().timeToDie).ToList();
+        population.Clear();
+        
+        //breed upper hald of sorted list
+        for (int i = (int) (sortedList.Count / 2.0f - 1); i < sortedList.Count - 1; i++){
+            population.Add(Breed(sortedList[i], sortedList[i + 1]));
+            population.Add(Breed(sortedList[i + 1], sortedList[i]));
+        }
+        // destroy all parents and previous population
+        for (int i = 0; i < sortedList.Count; i++){
+            Destroy(sortedList[i]);
+        }
+        generation++;
     }
 
     // Update is called once per frame
